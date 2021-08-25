9bdf34de0db4fd54bca0fca622e09fe93330e63e
mijeongseo <mimiseo@naraspace.com>
2021-08-26 08:16:41 +0900

    update

--- front-test/config/config.js
+++ front-test/config/config.js
@@ -1,2 +1,2 @@
-// export const backUrl = 'http://3.34.191.17';
-export const backUrl = "http://localhost:3065";
+export const backUrl = "http://3.36.124.171";
+// export const backUrl = "http://localhost:3065";
